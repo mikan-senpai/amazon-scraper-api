8708c1b5c4c08c66ac7b7e320c5f80a5be398b07
mikan <santanud5d80@gmail.com>
2022-06-20 17:42:25 +0530

    all the end points are created and test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to amazon scapper api ");
 });
 
-//get product details
+//GET PRODUCT DETAILS
 app.get("/products/:productId", async (req, res) => {
   const { productId } = req.params;
 
@@ -22,7 +22,52 @@ app.get("/products/:productId", async (req, res) => {
       `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
     );
 
-    res.json(response);
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET PRODUCT REVIEWS
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET PRODUCT OFFERS
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//(GET SEARCH RESULTS)=> IMPLEMENTING THE SERACH QUERY PRESENT IN AMAZON
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
   } catch (error) {
     res.json(error);
   }
